15db1c95530d470d5f1e3c9b24d123544b1c0617
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-09 16:06:35 -0700

    Step 161

---
+++ Methods1/Methods1/Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods1
+{
+    public class Numbers
+    {
+        public int NumOne { get; set; }
+        public static Numbers Multiply(Numbers nums)
+        {
+            nums.NumOne = nums.NumOne * 5;
+            return nums;
+        }
+        public static Numbers Add(Numbers nums)
+        {
+            nums.NumOne = nums.NumOne + 5;
+            return nums;
+        }
+        public static Numbers Subtract(Numbers nums)
+        {
+            nums.NumOne = nums.NumOne - 15;
+            return nums;
+        }
+    }
+
+   
+}
---
+++ Methods1/Methods1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers nums = new Numbers();
+            Console.WriteLine("Please enter a number: ");
+            nums.NumOne = Convert.ToInt32(Console.ReadLine());
+            nums = Numbers.Multiply(nums);
+            Console.WriteLine("Times 5: " + nums.NumOne);
+            nums = Numbers.Add(nums);
+            Console.WriteLine("Plus 5: " + nums.NumOne);
+            nums = Numbers.Subtract(nums);
+            Console.WriteLine("Minus 15: " + nums.NumOne);
+            Console.ReadLine();
+        }
+        
+    }
+}
