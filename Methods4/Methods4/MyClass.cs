810fe4679550c4231cc667d85d13c43fcc9a7923
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-10 10:27:27 -0700

    Step 166

---
+++ Methods4/Methods4/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    public class MyClass
+    {
+        public static void MyMethod(int i, int j)
+        {
+            i = i + 5;
+            j = i * j;
+            Console.WriteLine(j);
+        }
+    }
+}
---
+++ Methods4/Methods4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            MyClass.MyMethod(4, 5);
+
+            MyClass.MyMethod(i: 4, j: 5);
+            Console.ReadLine();
+        }
+    }
+}
