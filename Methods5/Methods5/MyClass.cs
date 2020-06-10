5cd4d54aa91083ac739b3f954db50c36816ca5fa
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-10 11:14:05 -0700

    Step 169

---
+++ Methods5/Methods5/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods5
+{
+     static class MyClass
+    {
+        public static void MyMethod(int i, out int j)
+        {
+            j = i / 2;
+        }
+
+        public static void MyMethod(int i, int j, out int k, out int l)
+        {
+            k = j - i;
+            l = j + i;
+        }
+    }
+}
---
+++ Methods5/Methods5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The below code will not work, as the class 'MyClass' is no longer declared public (now static per instructions).
+            //MyClass myClass = new MyClass();
+
+            Console.WriteLine("Please enter a number:");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            int numTwo;
+            MyClass.MyMethod(i: numOne, out numTwo);
+            Console.WriteLine("Number divided by two is: " + numTwo);
+
+            int numThree;
+            int numFour;
+            MyClass.MyMethod(i: numOne, j: numTwo, out numThree, out numFour);
+            Console.WriteLine("Quotient minus dividend: " + numThree);
+            Console.WriteLine("Quotient plus dividend: " + numFour);
+            Console.ReadLine();
+
+        }
+    }
+}
