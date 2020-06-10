c8bbb61092f18789a86492c0235a0035f98aa355
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-09 21:08:27 -0700

    Step 164

---
+++ Methods2/Methods2/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    public class MyClass
+    {
+        public static int Addition(int i)
+        {
+            i = i + 5;
+            i = i * 3;
+            return i;
+        }
+
+        public static decimal Addition(decimal i)
+        {
+            i = i * 3;
+            i = i / 2;
+            return i;
+        }
+
+        public static int Addition(string h)
+        {
+            int j = Convert.ToInt32(h) * 10;
+            return j;
+        }
+    }
+}
---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass HelloWorld = new MyClass();
+
+            Console.WriteLine(MyClass.Addition(3));
+            Console.ReadLine();
+
+            Console.WriteLine(MyClass.Addition(3.45m));
+            Console.ReadLine();
+
+            Console.WriteLine(MyClass.Addition("3"));
+            Console.ReadLine();
+        }
+    }
+}
