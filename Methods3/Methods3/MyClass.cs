ac47ca9d511058ab1c5c6e144ebf06802cce0d38
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-10 10:15:36 -0700

    Step 165

---
+++ Methods3/Methods3/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class MyClass
+    {
+        public static int MyMethod(int i, int j = 0)
+        {
+            i = i * j;
+            return i;
+        }
+    }
+}
---
+++ Methods3/Methods3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            try
+            {
+                Console.WriteLine("Please enter two numbers. (Entering a second number is optional. Default is 0)");
+                Console.WriteLine("First number:");
+                int numOne;
+                numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Second number:");
+                string numTwo = Console.ReadLine();
+
+                if (numTwo == "")
+                {
+                    Console.WriteLine(MyClass.MyMethod(numOne));
+                }
+                else
+                {
+                    Console.WriteLine(MyClass.MyMethod(numOne, Convert.ToInt32(numTwo)));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
