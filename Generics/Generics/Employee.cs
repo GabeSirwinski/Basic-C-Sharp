f934f5d0ae4c1058017fa408c18cf15409fd0e7d
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-11 11:18:07 -0700

    Step 190

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+        public static void PrintAll(Employee<T> employee)
+        {
+            foreach (T thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using Strings
+
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string>() { "Hello", "How", "Are", "You?" };
+            Employee<string>.PrintAll(employee1);
+
+            //Using Integers
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            Employee<int>.PrintAll(employee2);
+
+            Console.ReadLine();
+        }
+    }
+}
