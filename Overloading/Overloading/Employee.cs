6043b163cd934334d2973c34fdd43a517ef22257
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-11 09:47:14 -0700

    Step 187

---
+++ Overloading/Overloading/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            if (emp1.Id == emp2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            if (emp1.Id == emp2.Id)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            Employee employee3 = new Employee();
+            employee1.Id = 10;
+            employee2.Id = 20;
+            employee3.Id = 10;
+
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 == employee3);
+            Console.ReadLine();
+        }
+    }
+}
