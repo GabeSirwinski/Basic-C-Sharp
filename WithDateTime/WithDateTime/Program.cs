5f41bc6b8dc24f7a9468933902dd2bba1ac60d34
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-15 09:47:14 -0700

    Step 222

---
+++ WithDateTime/WithDateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = new DateTime();
+            current = DateTime.Now;
+            Console.WriteLine("Current DateTime: " + current + "\nPlease enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Current DateTime plus {0} hours = {1}.", hours, current.AddHours(hours));
+            Console.ReadLine();
+        }
+    }
+}
