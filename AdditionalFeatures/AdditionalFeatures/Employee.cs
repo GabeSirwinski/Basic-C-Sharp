749a3a85fb10f9fa44c02da5edd99145c1ca51da
GabeSirwinski <gabrielczerwinski@live.com>
2020-06-15 11:31:10 -0700

    230

---
+++ AdditionalFeatures/AdditionalFeatures/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    public class Employee
+    {
+        public Employee(int id, string name) : this(id, name, 20)
+        {
+        }
+        public Employee(int id, string name, int wage)
+        {
+            ID = id;
+            Name = name;
+            hourlyWage = wage;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int hourlyWage { get; set; }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int hoursInDay = 24;
+            Console.WriteLine("There are {0} hours in a day.",hoursInDay);
+            Console.ReadLine();
+            Employee employee1 = new Employee(1, "John", 25);
+            Employee employee2 = new Employee(2, "Jack");
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(employee1);
+            employees.Add(employee2);
+
+            var notSureWhatThisIs = employees.Where(x => x.ID < 3); // Example of using var
+
+            foreach (Employee employee in notSureWhatThisIs)
+            {
+                Console.WriteLine("Name: {0}.\nID: {1}.\nWage: ${2}/hour.\n", employee.Name, employee.ID, employee.hourlyWage);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
